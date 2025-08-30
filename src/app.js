5d234d7f4c3de52a796ce19e01139466a3d8835c
Farhan Sadik <farhansadik0760@gmail.com>
2025-08-30 19:18:38 +0600

    deploy url add

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: "https://auto-caption-three.vercel.app", 
     credentials: true,
   })
 );
